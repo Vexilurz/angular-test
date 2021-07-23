85a8268add2fbd2fda719c8d7d3a48e6dd7407ad
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 16:16:52 +0300

    3.2 Parameters pass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface IPost {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-
+  posts: IPost[] = [
+    {title: 'title 1', text: 'qwe qwe qwe qwe', id: 1},
+    {title: 'title 2', text: 'dsfwrgwg ewf f ew qe', id: 2},
+  ]
 
 }
