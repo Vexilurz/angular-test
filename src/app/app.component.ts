7fbf0cc0843f1292058a8b3eaa21e13650e0d32c
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 13:55:31 +0300

    2.11 ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  title = 'initial'
-
-  // onInput(event: any) {
-  //   this.title = event.target.value
-  // }
+  bgToggle = false
 
 }
