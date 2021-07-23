8b1d5cb872a46a7eedacb85a80eb5023b7e3f453
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 19:52:47 +0300

    4.4 Pass params into directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
 
+  @Input('appStyle') color: string = 'blue';
+  @Input() fontWeight: string = 'normal';
+
   constructor(private el: ElementRef, private r: Renderer2) {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
   }  
 
   @HostListener('click', ['$event.target']) onClick(event: Event) {
@@ -14,10 +16,12 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'red')
+    this.r.setStyle(this.el.nativeElement, 'color', this.color)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
   }
 }
