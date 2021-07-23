96916113a61b36f7ee1eaf822d06c10f874d37f5
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 21:19:40 +0300

    5.8 Filter list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+
+export interface Post {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  search = ''
+  searchField: keyof Post = 'title'
   
-  e: number = Math.E
-
-  str: string = 'hello world'
-
-  date: Date = new Date()
-
+  posts: Post[] = [
+    {title: 'Beer', text: 'Best beer ever'},
+    {title: 'Bread', text: 'asd qr qw fsdfeqwf qqwd qwd  fd'},
+    {title: 'JS', text: 'asfdgweewg rw  weff ewfd wef we'},
+  ]
 }
