aba6ad0709a0fbd4daab7e057de1f0d96638ea45
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 20:44:36 +0300

    5.7 Own pipe 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    MultByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
