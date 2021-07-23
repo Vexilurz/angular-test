9604583f0b08805028ec82ab3579c0826ec6e03e
Sergei Potekhin <vexilurz@gmail.com>
2021-07-23 19:57:51 +0300

    4.4 Pass styles as object

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -6,8 +6,7 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/
 export class StyleDirective {
 
   @Input('appStyle') color: string = 'blue';
-  @Input() fontWeight: string = 'normal';
-
+  @Input() dStyles!: { border?: string; fontWeight?: string; borderRadius?: string; };
   constructor(private el: ElementRef, private r: Renderer2) {
   }  
 
@@ -17,11 +16,15 @@ export class StyleDirective {
 
   @HostListener('mouseenter') onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', this.color)
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null)
     this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+    this.r.setStyle(this.el.nativeElement, 'border', null)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 }
