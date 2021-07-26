ebf8f27e534dfbf433273183275fafa6f270ca38
Sergei Potekhin <vexilurz@gmail.com>
2021-07-26 23:53:35 +0300

    14.2 two styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
-import { Meta, Title } from '@angular/platform-browser';
+import { state, style, trigger } from '@angular/animations';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({ background: 'blue' })),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
 
-  
-  
+  boxState = 'start';
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
+
 }
 
