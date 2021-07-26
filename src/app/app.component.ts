a66e37a37e1ee40eb787bd900da731bfde0ed87c
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 00:50:16 +0300

    14.10 ng-animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,12 @@
-import { animate, state, style, transition, trigger, group, query, keyframes, AnimationEvent } from '@angular/animations';
+import { AnimationEvent } from '@angular/animations';
 import { Component } from '@angular/core'
+import { boxAnimation } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('box', [
-      state('start', style({ background: 'blue' })),
-      state('end', style({
-        background: 'red',
-        transform: 'scale(1.2)'
-      })),
-      state('special', style({
-        background: 'orange',
-        transform: 'scale(0.5)',
-        borderRadius: '50%'
-      })),
-      transition('start => end', animate(500)),
-      transition('end => start', animate('800ms ease-in-out')),
-      transition('special <=> *', [
-        group([
-          query('h4', animate(1500, style({
-            fontSize: '.5rem',
-            color: 'lime'
-          }))),
-          style({background: 'green'}),
-          animate('1s', style({
-            background: 'pink'
-          })),
-          animate(750)
-        ])
-      ]),
-      // void => *
-      transition(':enter', [
-        animate('4s', keyframes([
-          style({background: 'red', offset: 0}),
-          style({background: 'black', offset: 0.2}),
-          style({background: 'orange', offset: 0.3}),
-          style({background: 'blue', offset: 1}),
-        ]))
-        // style({
-        //   opacity: 0
-        // }),
-        // animate('850ms ease-out')
-      ]),
-      // * => void
-      transition(':leave', [
-        style({
-          opacity: 1
-        }),
-        group([
-          animate(750, style({
-            opacity: 0,
-            transform: 'scale(1.5)'
-          })),
-          animate(300, style({
-            color: 'black',
-            fontWeight: 'bold'
-          }))
-        ])
-      ])
-    ])
-  ]
+  animations: [boxAnimation]
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimateComponent } from './animate/animate.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    AnimateComponent,
   ],
   imports: [
     BrowserModule,
