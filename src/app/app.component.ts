bef495788cb9ec8fc3f1b4e5e70f89cadfb2fe53
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 00:30:42 +0300

    14.8 animation steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger, group, query } from '@angular/animations';
+import { animate, state, style, transition, trigger, group, query, keyframes } from '@angular/animations';
 import { Component } from '@angular/core'
 
 @Component({
@@ -34,10 +34,16 @@ import { Component } from '@angular/core'
       ]),
       // void => *
       transition(':enter', [
-        style({
-          opacity: 0
-        }),
-        animate('850ms ease-out')
+        animate('4s', keyframes([
+          style({background: 'red', offset: 0}),
+          style({background: 'black', offset: 0.2}),
+          style({background: 'orange', offset: 0.3}),
+          style({background: 'blue', offset: 1}),
+        ]))
+        // style({
+        //   opacity: 0
+        // }),
+        // animate('850ms ease-out')
       ]),
       // * => void
       transition(':leave', [
