6cb1ac36e3081402324bf4a0e036ca77a1d6bb6c
Sergei Potekhin <vexilurz@gmail.com>
2021-07-26 12:34:35 +0300

    7.1 Forms template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AppCounterService } from './services/app-counter.service';
-import { LocalCounterService } from './services/local-counter.service';
-
-export interface Post {
-  title: string
-  text: string
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [LocalCounterService]
 })
 export class AppComponent {
 
   constructor(
-    public appCounterService: AppCounterService,
-    public localCounterService: LocalCounterService
   ) {
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
