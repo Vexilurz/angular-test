5666ab884257cff6557ecf8890016d980baceb35
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 00:18:36 +0300

    14.5 enter and leave

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,12 +26,30 @@ import { Component } from '@angular/core'
         })),
         animate(750)
       ]),
+      // void => *
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate('850ms ease-out')
+      ]),
+      // * => void
+      transition(':leave', [
+        style({
+          opacity: 1
+        }),
+        animate(750, style({
+          opacity: 0,
+          transform: 'scale(1.5)'
+        }))
+      ])
     ])
   ]
 })
 export class AppComponent {
 
   boxState = 'start';
+  visible = true;
 
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end'
